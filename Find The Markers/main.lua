2e0edc8131c2fae14e6e56120ba5f75e84200bf0
Babyhamsta <Babyhamstaz@yahoo.com>
2022-03-06 21:20:25 -0600

    Update main.lua

--- Find The Markers/main.lua
+++ Find The Markers/main.lua
@@ -1,3 +1,4 @@
+-- Plr Vars
 local Plr = game:GetService("Players").LocalPlayer
 local Char = Plr.Character
 
@@ -41,7 +42,43 @@ if game.PlaceId == 7896264844 then
         local TP = game:GetService("Workspace").MedievalTP
         repeat wait(); Char.HumanoidRootPart.CFrame = TP.CFrame  until false; -- never stop
     end)
+    
+    AutoSection:AddButton("Collect Special Markers/Badges", "Auto collects special markers and badges.", function()
+        -- Remotes
+        local Replicated = game:GetService("ReplicatedStorage")
+        local Get = Replicated.Get
+        local Hi = Replicated.Hi
+        local Technical = Replicated.Technical
+        local Stack = Replicated.Stack
+        local Pray = Replicated.Pray
+        
+        -- Collect some remote markers
+        Hi:FireServer();
+        Technical:FireServer();
+        Stack:FireServer();
+        Pray:FireServer();
+        
+        local winningbutton = game:GetService("Workspace")["Winning Marker"]["Color 1"]
+        Char.HumanoidRootPart.CFrame = winningbutton.CFrame
+        wait(0.1)
+        Get:FireServer(game.ReplicatedStorage.Markers["Winning Marker"]);
+        wait(2)
+        
+        local invertedbttn = game:GetService("Workspace")["Inverted Marker Button"]
+        Char.HumanoidRootPart.CFrame = invertedbttn.CFrame
+        wait(2)
+        
+        -- collect inverted
+        local invertedmarker = game:GetService("Workspace")["Inverted Marker"]["Color 1"]
+        Get:FireServer(game.ReplicatedStorage.Markers["Inverted Marker"]);
+        wait(2)
+        
+        local Explode = game:GetService("Workspace").ExplodeButton.Button;
+        Char.HumanoidRootPart.CFrame = Explode.CFrame
+        fireclickdetector(Explode.ClickDetector);
+    end)
 elseif game.PlaceId == 8506260179 then
+    game.ReplicatedStorage.Merchant:FireServer();
     TPSection:AddButton("Teleport To Main Game", "Teleports you back to the main game.", function()
         local TP = game:GetService("Workspace").Medieval.Go
         repeat wait(); Char.HumanoidRootPart.CFrame = TP.CFrame  until false; -- never stop

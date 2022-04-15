690fe88986737f7114217dac122156153fce73c4
Babyhamsta <Babyhamstaz@yahoo.com>
2022-04-15 17:25:05 -0500

    Update Bypasses.lua
    Updated ContentProvider bypass and added Lego's legit gcinfo bypass.

--- Universal/Bypasses.lua
+++ Universal/Bypasses.lua
@@ -1,38 +1,74 @@
 -- Pretty much just a bunch of know detection bypasses. (Big thanks to Lego Hacker, Modulus, and Bluwu)
 
--- GCInfo/CollectGarbage Bypass (Inspired by Lego)
+-- GCInfo/CollectGarbage Bypass (Realistic by Lego - Amazing work!)
 spawn(function()
-    repeat wait() until game:IsLoaded()
+    repeat task.wait() until game:IsLoaded()
 
-    local CurrGC = gcinfo();
-    local Rand = 0
-    local RunService = game:GetService("RunService")
+    local Amplitude = 200
+    local RandomValue = {-15,15}
+    local RandomTime = {.5, 1.5}
 
-    RunService.Stepped:Connect(function()
-        Rand = math.random(-200,200)
+    local floor = math.floor
+    local cos = math.cos
+    local sin = math.sin
+    local acos = math.acos
+    local pi = math.pi
+
+    local Maxima = 0
+
+    --Waiting for gcinfo to decrease
+    while task.wait() do
+        if gcinfo() >= Maxima then
+            Maxima = gcinfo()    
+        else
+            break    
+        end    
+    end
+
+    task.wait(0.30)
+
+    local OldGcInfo = gcinfo()+Amplitude
+    local tick = 0
+
+    --Spoofing gcinfo
+    local Old; Old = hookfunction(gcinfo, function(...)
+        local Formula = ((acos(cos(pi * (tick)))/pi * (Amplitude * 2)) + -Amplitude ) 
+        return floor(OldGcInfo + Formula) 
     end)
-
-    local GCINFO_Hook;
-    GCINFO_Hook = hookfunction(gcinfo, function(...)
-        if not checkcaller() then
-            return CurrGC + Rand;
+    local Old2; Old2 = hookfunction(collectgarbage, function(arg, ...)
+        if arg == "collect" then
+            return gcinfo(...)
         end
-        return GCINFO_Hook(...)
+        return Old2(arg, ...)
     end)
-     
-    local GC_Hook;
-    GC_Hook = hookfunction(collectgarbage, function(...)
-        local args = ...;
-        if not checkcaller() and args == "count" then
-            return CurrGC + Rand;
+
+
+    game:GetService("RunService").Stepped:Connect(function()
+        local Formula = ((acos(cos(pi * (tick)))/pi * (Amplitude * 2)) + -Amplitude ) 
+        if Formula > ((acos(cos(pi * (tick)+.01))/pi * (Amplitude * 2)) + -Amplitude ) then
+            tick = tick + .07
+        else
+            tick = tick + 0.01
         end
-        return GC_Hook(...)
     end)
+
+    local old1 = Amplitude
+    for i,v in next, RandomTime do
+        RandomTime[i] = v * 10000
+    end
+
+    local RandomTimeValue = math.random(RandomTime[1],RandomTime[2])/10000
+
+    --I can make it 0.003 seconds faster, yea, sure
+    while wait(RandomTime) do
+        Amplitude = math.random(old1+RandomValue[1], old1+RandomValue[2])
+        RandomTimeValue = math.random(RandomTime[1],RandomTime[2])/10000
+    end
 end)
 
 -- Memory Bypass
 spawn(function()
-    repeat wait() until game:IsLoaded()
+    repeat task.wait() until game:IsLoaded()
 
     local RunService = game:GetService("RunService")
     
@@ -70,9 +106,14 @@ end
 local ContentProviderBypass
 ContentProviderBypass = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
     local method = getnamecallmethod();
-
+    local args = ...;
+    
     if not checkcaller() and (method == "preloadAsync" or method == "PreloadAsync") and self:IsA("ContentProvider") then
-        return;
+        if args[1] ~= nil then
+            if type(args[1]) == "table" then
+                return;
+            end
+        end
     end
 
     return ContentProviderBypass(self, ...)

31006e10fd3459070edc6acd4a89795c08d231eb
Babyhamsta <Babyhamstaz@yahoo.com>
2022-04-23 16:58:48 -0500

    Update BypassedDarkDexV3.lua
    Made Dex a global for easy access from bypasses.lua

--- Universal/BypassedDarkDexV3.lua
+++ Universal/BypassedDarkDexV3.lua
@@ -18,7 +18,7 @@ CloneRef (Overall protection of Instances and other UserData)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/Bypasses.lua", true))()
 
 -- Dex with CloneRef Support
-local Dex = game:GetObjects("rbxassetid://9352453730")[1]
+getgenv().Dex = game:GetObjects("rbxassetid://9352453730")[1]
 math.randomseed(tick())
 
 local charset = {}

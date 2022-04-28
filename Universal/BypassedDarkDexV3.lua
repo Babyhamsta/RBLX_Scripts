1166bab7413a6e9bf02c9a2f3e2145883f8681e2
Babyhamsta <Babyhamstaz@yahoo.com>
2022-04-27 21:24:28 -0500

    Update BypassedDarkDexV3.lua
    Updated CoreGui to use cloneref.

--- Universal/BypassedDarkDexV3.lua
+++ Universal/BypassedDarkDexV3.lua
@@ -18,6 +18,9 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scrip
 getgenv().Bypassed_Dex = game:GetObjects("rbxassetid://9352453730")[1]
 math.randomseed(tick())
 
+-- CoreGui
+local CoreGui = cloneref(game:GetService("CoreGui"))
+
 local charset = {}
 for i = 48,  57 do table.insert(charset, string.char(i)) end
 for i = 65,  90 do table.insert(charset, string.char(i)) end
@@ -31,7 +34,7 @@ function RandomCharacters(length)
 end
 
 Bypassed_Dex.Name = RandomCharacters(math.random(5, 20))
-Bypassed_Dex.Parent = game:GetService("CoreGui")
+Bypassed_Dex.Parent = CoreGui
 
 getcustomasset = getsynasset or getcustomasset
 base64decode = syn_crypt_b64_decode or (crypt and crypt.base64decode)
@@ -41,7 +44,7 @@ if getcustomasset and readfile then
 		pcall(function()
 			local o = base64decode(game:GetObjects("rbxassetid://6325145856")[1].x.Source)
 			writefile("cx.ogg",o)
-			local s = Instance.new("Sound",game:GetService("CoreGui"))
+			local s = Instance.new("Sound", CoreGui)
 			s.Name = game:GetService("HttpService"):GenerateGUID()
 			s.SoundId = getcustomasset("cx.ogg")
 			s.Volume = 2

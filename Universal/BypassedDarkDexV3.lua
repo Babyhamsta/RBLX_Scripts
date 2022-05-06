07e23e18ba2bcbc9e2978fdeb3661bac15abb856
Babyhamsta <Babyhamstaz@yahoo.com>
2022-05-05 22:49:41 -0500

    Update BypassedDarkDexV3.lua
    Fixed Formatting

--- Universal/BypassedDarkDexV3.lua
+++ Universal/BypassedDarkDexV3.lua
@@ -22,50 +22,50 @@ for i = 48,  57 do table.insert(charset, string.char(i)) end
 for i = 65,  90 do table.insert(charset, string.char(i)) end
 for i = 97, 122 do table.insert(charset, string.char(i)) end
 function RandomCharacters(length)
-  if length > 0 then
-    return RandomCharacters(length - 1) .. charset[math.random(1, #charset)]
-  else
-    return ""
-  end
+    if length > 0 then
+        return RandomCharacters(length - 1) .. charset[math.random(1, #charset)]
+    else
+        return ""
+    end
 end
 
 Bypassed_Dex.Name = RandomCharacters(math.random(5, 20))
 Bypassed_Dex.Parent = cloneref(game:GetService("CoreGui"))
 
 local function Load(Obj, Url)
-	local function GiveOwnGlobals(Func, Script)
-		local Fenv = {}
-		local RealFenv = {script = Script}
-		local FenvMt = {}
-		FenvMt.__index = function(a,b)
-			if RealFenv[b] == nil then
-				return getfenv()[b]
-			else
-				return RealFenv[b]
-			end
-		end
-		FenvMt.__newindex = function(a, b, c)
-			if RealFenv[b] == nil then
-				getfenv()[b] = c
-			else
-				RealFenv[b] = c
-			end
-		end
-		setmetatable(Fenv, FenvMt)
-		setfenv(Func, Fenv)
-		return Func
-	end
-	
-	local function LoadScripts(Script)
-		if Script.ClassName == "Script" or Script.ClassName == "LocalScript" then
-			spawn(GiveOwnGlobals(loadstring(Script.Source, "=" .. Script:GetFullName()), Script))
-		end
-		for i,v in pairs(Script:GetChildren()) do
-			LoadScripts(v)
-		end
-	end
-	
-	LoadScripts(Obj)
+    local function GiveOwnGlobals(Func, Script)
+        local Fenv = {}
+        local RealFenv = {script = Script}
+        local FenvMt = {}
+        FenvMt.__index = function(a,b)
+            if RealFenv[b] == nil then
+                return getfenv()[b]
+            else
+                return RealFenv[b]
+            end
+        end
+        FenvMt.__newindex = function(a, b, c)
+            if RealFenv[b] == nil then
+                getfenv()[b] = c
+            else
+                RealFenv[b] = c
+            end
+        end
+        setmetatable(Fenv, FenvMt)
+        setfenv(Func, Fenv)
+        return Func
+    end
+
+    local function LoadScripts(Script)
+        if Script.ClassName == "Script" or Script.ClassName == "LocalScript" then
+            spawn(GiveOwnGlobals(loadstring(Script.Source, "=" .. Script:GetFullName()), Script))
+        end
+        for i,v in pairs(Script:GetChildren()) do
+            LoadScripts(v)
+        end
+    end
+
+    LoadScripts(Obj)
 end
 
 Load(Bypassed_Dex)

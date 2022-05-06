cfd7c5afafe7db2829f34979909f096b70d83ecf
Babyhamsta <Babyhamstaz@yahoo.com>
2022-05-05 22:48:23 -0500

    Update BypassedDarkDexV3.lua
    Removed random bits that may cause detection.

--- Universal/BypassedDarkDexV3.lua
+++ Universal/BypassedDarkDexV3.lua
@@ -14,12 +14,8 @@ CloneRef (Overall protection of Instances and other UserData)
 -- Dex Bypasses
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/Bypasses.lua", true))()
 
--- Dex with CloneRef Support
+-- Dex with CloneRef Support (made as global)
 getgenv().Bypassed_Dex = game:GetObjects("rbxassetid://9352453730")[1]
-math.randomseed(tick())
-
--- CoreGui
-local CoreGui = cloneref(game:GetService("CoreGui"))
 
 local charset = {}
 for i = 48,  57 do table.insert(charset, string.char(i)) end
@@ -34,26 +30,7 @@ function RandomCharacters(length)
 end
 
 Bypassed_Dex.Name = RandomCharacters(math.random(5, 20))
-Bypassed_Dex.Parent = CoreGui
-
-getcustomasset = getsynasset or getcustomasset
-base64decode = syn_crypt_b64_decode or (crypt and crypt.base64decode)
-if getcustomasset and readfile then
-	local s,res = pcall(readfile,"\67\77\68\45\88\46\108\117\97")
-	if s and res and res ~= "" then
-		pcall(function()
-			local o = base64decode(game:GetObjects("rbxassetid://6325145856")[1].x.Source)
-			writefile("cx.ogg",o)
-			local s = Instance.new("Sound", CoreGui)
-			s.Name = game:GetService("HttpService"):GenerateGUID()
-			s.SoundId = getcustomasset("cx.ogg")
-			s.Volume = 2
-			s.PlayOnRemove = true
-			wait()
-			s:Destroy()
-		end)
-	end
-end
+Bypassed_Dex.Parent = cloneref(game:GetService("CoreGui"))
 
 local function Load(Obj, Url)
 	local function GiveOwnGlobals(Func, Script)

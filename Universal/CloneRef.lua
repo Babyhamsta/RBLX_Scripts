dbf56211cb6109bb92104645aa823e7da19d568f
Babyhamsta <Babyhamstaz@yahoo.com>
2022-05-21 10:49:43 -0500

    Update CloneRef.lua

--- Universal/CloneRef.lua
+++ Universal/CloneRef.lua
@@ -1,4 +1,4 @@
 -- Add cloneref support if the exploit doesn't have it or if it's really shit..
 
 -- Credit to Alex | JJsploit/Electron Support
-local a=Instance.new("Part")for b,c in pairs(getreg())do if type(c)=="table"and#c then if rawget(c,"__mode")=="kvs"then for d,e in pairs(c)do if e==a then getgenv().InstanceList=c;break end end end end end;function ghetto_cloneref(f)if not InstanceList then return end;for b,c in pairs(InstanceList)do if c==f then InstanceList[b]=nil;break end end end;if not cloneref or IsElectron then getgenv().cloneref=ghetto_cloneref end
+local a=Instance.new("Part")for b,c in pairs(getreg())do if type(c)=="table"and#c then if rawget(c,"__mode")=="kvs"then for d,e in pairs(c)do if e==a then getgenv().InstanceList=c;break end end end end end;function ghetto_cloneref(f)if not InstanceList then return end;for b,c in pairs(InstanceList)do if c==f then InstanceList[b]=nil;break end end end;if not cloneref then getgenv().cloneref=ghetto_cloneref end

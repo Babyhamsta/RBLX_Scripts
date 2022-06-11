bcb7871ec75dbe134130de9b9edf7cd2568d7476
Babyhamsta <Babyhamstaz@yahoo.com>
2022-06-11 15:40:20 -0500

    Create SimpleESP.lua

---
+++ Universal/SimpleESP.lua
@@ -0,0 +1,50 @@
+function Create(base, team, name, trackername, color, studs)
+    local bb = Instance.new('BillboardGui', game.CoreGui)
+    bb.Adornee = base
+    bb.ExtentsOffset = Vector3.new(0,1,0)
+    bb.AlwaysOnTop = true
+    bb.Size = UDim2.new(0,6,0,6)
+    bb.StudsOffset = Vector3.new(0,1,0)
+    bb.Name = trackername
+
+    local frame = Instance.new('Frame', bb)
+    frame.ZIndex = 10
+    frame.BackgroundTransparency = 0.3
+    frame.Size = UDim2.new(1,0,1,0)
+    frame.BackgroundColor3 = color
+
+    local txtlbl = Instance.new('TextLabel', bb)
+    txtlbl.ZIndex = 10
+    txtlbl.BackgroundTransparency = 1
+    txtlbl.Position = UDim2.new(0,0,0,-48)
+    txtlbl.Size = UDim2.new(1,0,10,0)
+    txtlbl.Font = 'ArialBold'
+    txtlbl.FontSize = 'Size12'
+    txtlbl.Text = name
+    txtlbl.TextStrokeTransparency = 0.5
+    txtlbl.TextColor3 = color
+
+    local txtlblstud = Instance.new('TextLabel', bb)
+    txtlblstud.ZIndex = 10
+    txtlblstud.BackgroundTransparency = 1
+    txtlblstud.Position = UDim2.new(0,0,0,-35)
+    txtlblstud.Size = UDim2.new(1,0,10,0)
+    txtlblstud.Font = 'ArialBold'
+    txtlblstud.FontSize = 'Size12'
+    txtlblstud.Text = tostring(studs) .. " Studs"
+    txtlblstud.TextStrokeTransparency = 0.5
+    txtlblstud.TextColor3 = Color3.new(255,255,255)
+end
+
+-- Clear ESP
+function ClearESP(espname)
+    for _,v in pairs(game.CoreGui:GetChildren()) do
+        if v.Name == espname and v:isA('BillboardGui') then
+            v:Destroy()
+        end
+    end
+end
+
+-- Example:
+-- Create(v.Character.Head, false, v.Name, "playertracker", TeamColor, math.floor(studs + 0.5))
+-- ClearESP("playertracker");

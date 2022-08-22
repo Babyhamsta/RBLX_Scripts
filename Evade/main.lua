cadfb493201766022ccf1ab0f24b22338c166ae0
Babyhamsta <Babyhamstaz@yahoo.com>
2022-08-21 21:33:01 -0500

    Create main.lua
    First Version, lets goooo

---
+++ Evade/main.lua
@@ -0,0 +1,193 @@
+-- [[ HamstaGang on V3RM | Last updated 08/21/2022 ]] --
+
+-- Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage");
+local CoreGui = game:GetService("CoreGui");
+local Players = game:GetService("Players");
+local Workspace = game:GetService("Workspace");
+local Lighting = game:GetService("Lighting");
+
+-- Remote Stuff
+local Events = ReplicatedStorage:WaitForChild("Events", 1337)
+
+-- Local Player
+local Player = Players.LocalPlayer;
+local Character = Player.Character or Player.CharacterAdded:Wait()
+
+-- UI Lib (Fluxus Lib because I like to shuffle them and they support WEAO <3)
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/UILibs/FluxusUI.lua"))()
+
+-- ESP support
+local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/SimpleHighlightESP.lua"))()
+
+-- Main Window
+local Window = lib:CreateWindow("Evade Gui")
+
+-- Create Pages
+local MainPage = Window:NewTab("Main")
+local ServerPage = Window:NewTab("Server")
+local ESPPage = Window:NewTab("ESP")
+
+-- Create Sections
+local MainSection = MainPage:AddSection("Main")
+local ServerSection = ServerPage:AddSection("Server")
+local ESPSection = ESPPage:AddSection("ESP")
+
+-- GUI Toggles
+local Highlights_Active = false;
+local AI_ESP = false;
+local GodMode_Enabled = false;
+
+-- Simple Text ESP
+function Simple_Create(base, name, trackername, studs)
+    local bb = Instance.new('BillboardGui', game.CoreGui)
+    bb.Adornee = base
+    bb.ExtentsOffset = Vector3.new(0,1,0)
+    bb.AlwaysOnTop = true
+    bb.Size = UDim2.new(0,6,0,6)
+    bb.StudsOffset = Vector3.new(0,1,0)
+    bb.Name = trackername
+
+    local frame = Instance.new('Frame', bb)
+    frame.ZIndex = 10
+    frame.BackgroundTransparency = 0.3
+    frame.Size = UDim2.new(1,0,1,0)
+    frame.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+
+    local txtlbl = Instance.new('TextLabel', bb)
+    txtlbl.ZIndex = 10
+    txtlbl.BackgroundTransparency = 1
+    txtlbl.Position = UDim2.new(0,0,0,-48)
+    txtlbl.Size = UDim2.new(1,0,10,0)
+    txtlbl.Font = 'ArialBold'
+    txtlbl.FontSize = 'Size12'
+    txtlbl.Text = name
+    txtlbl.TextStrokeTransparency = 0.5
+    txtlbl.TextColor3 = Color3.fromRGB(255, 0, 0)
+
+    local txtlblstud = Instance.new('TextLabel', bb)
+    txtlblstud.ZIndex = 10
+    txtlblstud.BackgroundTransparency = 1
+    txtlblstud.Position = UDim2.new(0,0,0,-35)
+    txtlblstud.Size = UDim2.new(1,0,10,0)
+    txtlblstud.Font = 'ArialBold'
+    txtlblstud.FontSize = 'Size12'
+    txtlblstud.Text = tostring(studs) .. " Studs"
+    txtlblstud.TextStrokeTransparency = 0.5
+    txtlblstud.TextColor3 = Color3.new(255,255,255)
+end
+
+-- Clear ESP
+function ClearESP(espname)
+    for _,v in pairs(game.CoreGui:GetChildren()) do
+        if v.Name == espname and v:isA('BillboardGui') then
+            v:Destroy()
+        end
+    end
+end
+
+-- Crash Server (Credits to FeIix (V3RM) for the idea <3)
+MainSection:AddToggle("God Mode", "Gives you god mode", false, function(bool)
+    GodMode_Enabled = bool;
+end)
+
+MainSection:AddButton("Alpha Skin", "Gives you the private alpha skin", function()
+    Events.UI.Purchase:InvokeServer("Skins", "AlphaTester")
+end)
+
+MainSection:AddButton("Full Bright", "For users who are scared of the dark :(", function()
+    local light = Instance.new("PointLight", Character.HumanoidRootPart)
+    light.Brightness = .3
+    light.Range = 10000
+
+    Lighting.TimeOfDay = "14:00:00"
+    Lighting.FogEnd = 10000;
+    Lighting.Brightness = 2;
+    Lighting.Ambient = Color3.fromRGB(255,255,255)
+    Lighting.FogColor = Color3.fromRGB(255,255,255)
+end)
+
+
+-- Crash Server (Credits to FeIix (V3RM) for the idea <3)
+ServerSection:AddButton("Crash Server", "Crashes the server", function()
+    local Respawn = Events:FindFirstChild("Respawn")
+    while task.wait() do
+        if Respawn then
+            Respawn:FireServer()
+        end
+    end
+end)
+
+
+-- Character Highlights
+ESPSection:AddButton("Character Highlights", "Highlights all characters to make them easier to see.", function()
+    ESP:ClearESP();
+    Highlights_Active = true;
+
+    for i, v in ipairs(Players:GetPlayers()) do
+        if v ~= Player then
+            v.CharacterAdded:Connect(function(Character)
+                ESP:AddOutline(Character)
+                ESP:AddNameTag(Character)
+            end)
+
+            if v.Character then
+                ESP:AddOutline(v.Character)
+                ESP:AddNameTag(v.Character)
+            end
+        end
+    end
+end)
+
+-- AI Text ESP
+ESPSection:AddToggle("AI ESP", "Adds text ESP to AI to make them easier to see.", false, function(bool)
+    AI_ESP = bool;
+end)
+
+-- Highlight helper
+game:GetService("Players").PlayerAdded:Connect(function(Player)
+    Player.CharacterAdded:Connect(function(Character)
+        if Highlights_Active then
+            ESP:AddOutline(Character)
+            ESP:AddNameTag(Character)
+        end
+    end)
+end)
+
+
+-- [[ LOOPS ]] --
+
+-- God Mode
+task.spawn(function()
+    while wait(3) do
+        if GodMode_Enabled then
+            pcall(function()
+                local Humanoid = Character:FindFirstChild("Humanoid");
+                Humanoid.Parent = nil;
+                Humanoid:Clone();
+                Humanoid.Name = "Humanoid";
+                Humanoid.Parent = Character;
+            end)
+        end
+    end
+end)
+
+-- ESP AI
+task.spawn(function()
+    while wait(0.3) do
+        if AI_ESP then
+            pcall(function()
+                ClearESP("AI_Tracker")
+                local GamePlayers = Workspace:WaitForChild("Game", 1337).Players;
+                for i,v in pairs(GamePlayers:GetChildren()) do
+                    if not game.Players:FindFirstChild(v.Name) then -- Is AI
+                        local studs = Player:DistanceFromCharacter(v.PrimaryPart.Position)
+                        Simple_Create(v.HumanoidRootPart, v.Name, "AI_Tracker", math.floor(studs + 0.5))
+                    end
+                end
+            end)
+        else
+            ClearESP("AI_Tracker");
+        end
+    end
+end)
